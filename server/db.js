2fdfacaa1f393d256b77b1009d77c1dbf2cff6a1
nathan <side_nathan@hotmail.com>
2023-09-07 02:38:02 -0400

    Connect front end to backend and db

--- server/db.js
+++ server/db.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const uri = process.env.MONGOURI;
+const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
 });
 
 const db = mongoose.connection;
 
-db.on("error", console.error("Connection failed"));
-db.op("open", () => {
+db.on("error", () => console.error("Connection failed"));
+db.on("open", () => {
   console.log("Connected to MongoDB Atlas");
 });
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const db = require("./db");
+const path = require("path");
+require("./db");
 
 const app = express();
+
+const buildDir = "../client/build";
+app.use(express.static(path.join(__dirname, buildDir)));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
-})
+});
